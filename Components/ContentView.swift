f4909c510c7107a6b6c3c3f4033dcca3e1f10ba9
Steve Tibbett <steve@fallday.ca>
2019-10-15 18:28:42 -0400

    Initial Commit

---
+++ Components/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Components
+//
+//  Created by Steve Tibbett on 2019-10-15.
+//  Copyright © 2019 Steve Tibbett. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
